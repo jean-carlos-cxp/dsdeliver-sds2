ac958322356c3da464a4c1dea418dc1d741525fc
Jean Carlos <carlosjean090@gmail.com>
2021-01-17 23:40:18 -0300

    finished project

--- front-mobile/src/OrderCard/index.tsx
+++ front-mobile/src/OrderCard/index.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
 import { StyleSheet, Text, View } from "react-native";
+import { Order } from '../types';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
-function OrderCard() {
+dayjs.locale('pt-br');
+dayjs.extend(relativeTime);
+
+type Props = {
+  order: Order;
+}
+
+function dateFromNow(date: string) {
+  return dayjs(date).fromNow();
+}
+
+/* function formatPrice(price: number) {
+   const formatter = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BLR',
+      minimumFractionDigits: 2
+  }); 
+
+  return formatter.format(price);
+} */
+
+function OrderCard({ order }: Props) {
     return (
-       <>
-         <View style={styles.container}>
-             <View style={styles.header}>
-                <Text style={styles.orderName}>Pedido 1</Text>
-                <Text style={styles.orderPrice}>R$ 50,00</Text>
-             </View>
-             <Text style={styles.text}>Há 30 minutos</Text>
-            <View style={styles.productsList}>
-                <Text style={styles.text}>Piazza Calabresa</Text>
-                <Text style={styles.text}>Piazza Quatro Queijos</Text>
-                <Text style={styles.text}>Piazza Marguerita</Text>
-            </View>
-         </View>
-       </>
+        <View style={styles.container}>
+          <View style={styles.header}>
+              <Text style={styles.orderName}>Pedido {order.id}</Text>
+              <Text style={styles.orderPrice}>R$ {order.total}</Text>
+           </View>
+              <Text style={styles.text}>{dateFromNow(order.moment)}</Text>
+           <View style={styles.productsList}>
+              {order.products.map(product => (
+               <Text key={product.id} style={styles.text}>{product.name}</Text>
+             ))}
+          </View>
+       </View>
     );
 }
 
